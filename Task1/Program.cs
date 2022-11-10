411e92d2372bcb95408d0b7276aab6ddacba9909
Zoia611 <117438266+Zoia611@users.noreply.github.com>
2022-11-10 13:31:37 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7];
+            int S = 0;
+            int k = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                S += array[i];
+                k++;
+            }
+            Console.WriteLine(S / k);
+            Console.ReadKey();
+
+        }
+    }
+}
